f4dd3ccf89e7f2d4d5181972b00f2203f51e936f
Admin <admin@Ellens-MacBook-Air-3.local>
2023-01-08 17:12:58 +0100

    counter

--- DiceGame/ContentView.swift
+++ DiceGame/ContentView.swift
@@ -10,17 +10,29 @@ import SwiftUI
 struct ContentView: View {
     @State var diceNumber1 = 4
     @State var diceNumber2 = 4
-
+    @State var sum = 0
     
     var body: some View {
         ZStack{
+            
             Color(red: 51/256, green: 106/256, blue: 61/256)
         
+                
+                
         VStack{
+            
+            Text("\(sum)")
+                .font(.title)
+                .foregroundColor(.white)
+                .fontWeight(.bold)
+            Spacer()
+
             HStack{
             DiceView(n: diceNumber1)
             DiceView(n: diceNumber2)
             }
+            
+            Spacer()
             Button(action: {
                 roll()
             }) {
@@ -32,7 +44,9 @@ struct ContentView: View {
             }
             .background(Color.red)
             .cornerRadius(15.0)
+            Spacer()
           }
+        
         }
     }
     
@@ -40,6 +54,7 @@ struct ContentView: View {
         diceNumber1 = Int.random(in: 1...6)
         diceNumber2 = Int.random(in: 1...6)
 
+        sum += diceNumber1+diceNumber2
     }
 }
 
