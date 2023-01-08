6905b9ec236e80bd9334eaf5108f80920a95317e
Admin <admin@Ellens-MacBook-Air-3.local>
2023-01-08 16:34:35 +0100

    two dices

--- DiceGame/ContentView.swift
+++ DiceGame/ContentView.swift
@@ -8,19 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var diceNumber = 4
+    @State var diceNumber1 = 4
+    @State var diceNumber2 = 4
+
     
     var body: some View {
         ZStack{
             Color(red: 51/256, green: 106/256, blue: 61/256)
         
         VStack{
-        
-        Image(systemName: "die.face.\(diceNumber)")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .padding()
-            
+            HStack{
+            DiceView(n: diceNumber1)
+            DiceView(n: diceNumber2)
+            }
             Button(action: {
                 roll()
             }) {
@@ -37,12 +37,25 @@ struct ContentView: View {
     }
     
     func roll() {
-        diceNumber = Int.random(in: 1...6)
-        
+        diceNumber1 = Int.random(in: 1...6)
+        diceNumber2 = Int.random(in: 1...6)
+
     }
 }
 
-
+struct DiceView : View {
+    let n: Int
+    
+    var body: some View{
+       
+        
+        Image(systemName: "die.face.\(n)")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .padding()
+    }
+    
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
